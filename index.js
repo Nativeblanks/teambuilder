ee38cf173e81059cdddad4e131a37d3147cb500c
Zach B <nativeblanks@gmail.com>
2021-10-07 20:53:29 -0400

    inquire logic to ask role specific questions

--- index.js
+++ index.js
@@ -26,16 +26,116 @@ function initialQuestion() {
 }
 
 
+function managerQuestions() {
+    return inquirer.prompt([
+
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your name: ",
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: "Enter your ID here: ",
+
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email: ",
+        },
+
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "Enter your office number:  ",
+
+        },
+
+
+    ])     
+}
+
+function engineerQuestions () {
+    return inquirer.prompt([
+
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your name: ",
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: "Enter your ID here: ",
+
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email: ",
+        },
+
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your github:  ",
+
+        },
+    ])   
+}
+
+function internQuestions () {
+    return inquirer.prompt([
+
+        {
+            type: "input",
+            name: "name",
+            message: "Enter your name: ",
+        },
+
+        {
+            type: "input",
+            name: "id",
+            message: "Enter your ID here: ",
+
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Enter your email: ",
+        },
+
+        {
+            type: "input",
+            name: "highschool",
+            message: "Enter your highschool:  ",
+        },
+    ])   
+}
+
 
 initialQuestion().then((answers) => {
     console.log(answers);
     console.log(answers.position)
     if (answers.position === "Manager") {
         console.log("manager logged");
+        managerQuestions().then((answers)=> {
+            console.log(answers)
+        })
+        
     } else if 
         (answers.position === "Engineer") {
             console.log("Engineer picked");
+            engineerQuestions().then((answers) => {
+                console.log(answers)
+            })
         } else {
         console.log("last resort is intern");
-    }
-})
+        internQuestions().then((answers) => {
+            console.log(answers)
+    })
+}})
